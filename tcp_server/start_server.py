1840f58269aed5cf71dabfe66993b027ceccfdc6
Manuel Ruiz Echazu <ruitzei@gmail.com>
2020-06-06 15:29:56 -0300

    Added file utils

--- tcp_server/start_server.py
+++ tcp_server/start_server.py
@@ -1,4 +1,4 @@
 def start_server(server_address, storage_dir):
-  # TODO: Implementar TCP server
-  print('TCP: start_server({}, {})'.format(server_address, storage_dir))
-  pass
+    # TODO: Implementar TCP server
+    print('TCP: start_server({}, {})'.format(server_address, storage_dir))
+    pass
