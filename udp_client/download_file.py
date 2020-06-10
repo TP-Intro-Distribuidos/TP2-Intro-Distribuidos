7412eef4fdc5578bfb9331e23ccf3c9cd4858afa
Franco Mazzoni <franco.mazzoni@despegar.com>
2020-06-10 17:57:57 -0300

    Fix in download in client udp

--- udp_client/download_file.py
+++ udp_client/download_file.py
@@ -16,25 +16,28 @@ def download_file(server_address, name, dst):
     print("Sending download command")
     # Send download command and wait for response
     response = send_message_with_retries(sock, server_address, (ActionType.DOWNLOAD.value + DELIMITER + name).encode())
-    print("Response {} ".format(response))
-    command, number_of_chunks = response.split(DELIMITER)
-    if command == ActionType.BEGIN_DOWNLOAD.value:
-        print("Begin Download command received from server: starting download")
-        chunks = receive_chunks(sock, server_address, number_of_chunks)
+    if response is not None:
+        print("Response {} ".format(response))
+        command, number_of_chunks = response.split(DELIMITER)
+        if command == ActionType.BEGIN_DOWNLOAD.value:
+            print("Begin Download command received from server: starting download")
+            chunks = receive_chunks(sock, server_address, number_of_chunks)
 
-        # parsing dir and filename
-        dir_and_filename = get_dir_and_filename(dst)
+            # parsing dir and filename
+            dir_and_filename = get_dir_and_filename(dst)
 
-        # Prepare the file
-        dir = dir_and_filename[0]
-        filename = dir_and_filename[1]
-        create_directory(dir)
-        file = open('{}/{}'.format(dir, filename), "w")
-        size = len(chunks)
-        for i in range(size):
-            file.write(chunks[str(i)])
-        file.close()
+            # Prepare the file
+            dir = dir_and_filename[0]
+            filename = dir_and_filename[1]
+            create_directory(dir)
+            file = open('{}/{}'.format(dir, filename), "w")
+            size = len(chunks)
+            for i in range(size):
+                file.write(chunks[str(i)])
+            file.close()
+        else:
+            print('Download failed. Retry')
     else:
-        print('Upload failed. Retry')
+        print('Download failed. Retry')
 
     sock.close()
